0f94f3e31e0d0c13ca436e2eac2015933b0883c8
WanB7 <141239330+WanB7@users.noreply.github.com>
2024-03-19 10:06:01 -0300

    Delete VitalHub directory

--- VitalHub/src/components/BtnAppointment/BtnAppointment.js
+++
@@ -1,9 +0,0 @@
-import { ButtonTab, ButtonText } from "./Style"
-
-export const BtnAppointment = ({clickButton = false, textButton, onPress}) => {
-    return(
-        <ButtonTab clickButton={clickButton} onPress={onPress}>
-            <ButtonText clickButton={clickButton}>{textButton}</ButtonText>
-        </ButtonTab>
-    )
-}
